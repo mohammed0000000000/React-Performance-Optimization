35409731d931c519350eb107b9325fd5928c1585
Muhammed Rede Maklad <mohammedmaklad97@gmail.com>
2024-07-10 20:40:37 +0300

    init project & first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="space-y-6">
+        <h1 className="font-semibold text-yellow-500 mb-10">
+          Performance & Optimization
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
